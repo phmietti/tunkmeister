8ae21c610e86444f634e53bb346d50e204d005bf
Petri Miettinen <ext-petri.miettinen@elisa.fi>
2016-05-16 10:32:08 +0300

    extracted FavoriteEvent to own file

---
+++ tunkmeister/tunkmeister/FavoriteEvent.swift
@@ -0,0 +1,44 @@
+//
+// Created by Petri Miettinen on 16/05/16.
+// Copyright (c) 2016 phmietti. All rights reserved.
+//
+
+import Foundation
+
+struct FavoriteEvent {
+    let startHour: Int
+    let startMinutes: Int
+    let endHour: Int
+    let endMinutes: Int
+    let title: String?
+
+    func encode() -> NSDictionary {
+        var dictionary: Dictionary = Dictionary<String, AnyObject>()
+        dictionary["startHour"] = startHour
+        dictionary["startMinutes"] = startMinutes
+        dictionary["endHour"] = endHour
+        dictionary["endMinutes"] = endMinutes
+        if let t = title {
+            dictionary["title"] = t
+        }
+        return dictionary
+    }
+
+    func matches(startDateMaybe: NSDate?, endDateMaybe: NSDate?, titleMaybe: String?) -> Bool {
+        if let startDate = startDateMaybe, endDate = endDateMaybe {
+            return startDate.hour() == startHour && startDate.minutes() == startMinutes && endDate.hour() == endHour && endDate.minutes() == endMinutes && titleMaybe == title
+        } else {
+            return false
+        }
+    }
+
+    // Decode
+    static func decode(dictionary: NSDictionary) -> FavoriteEvent {
+        let startHour = dictionary["startHour"] as! Int
+        let startMinutes = dictionary["startMinutes"] as! Int
+        let endHour = dictionary["endHour"] as! Int
+        let endMinutes = dictionary["endMinutes"] as! Int
+        let title = dictionary["title"] as? String
+        return FavoriteEvent(startHour: startHour, startMinutes: startMinutes, endHour: endHour, endMinutes: endMinutes, title: title)
+    }
+}
--- tunkmeister/tunkmeister/ViewController.swift
+++ tunkmeister/tunkmeister/ViewController.swift
@@ -40,44 +40,6 @@ class FavoriteEventCell: UICollectionViewCell {
 
 }
 
-struct FavoriteEvent {
-    let startHour: Int
-    let startMinutes: Int
-    let endHour: Int
-    let endMinutes: Int
-    let title: String?
-
-    func encode() -> NSDictionary {
-        var dictionary: Dictionary = Dictionary<String, AnyObject>()
-        dictionary["startHour"] = startHour
-        dictionary["startMinutes"] = startMinutes
-        dictionary["endHour"] = endHour
-        dictionary["endMinutes"] = endMinutes
-        if let t = title {
-            dictionary["title"] = t
-        }
-        return dictionary
-    }
-
-    func matches(startDateMaybe: NSDate?, endDateMaybe: NSDate?, titleMaybe: String?) -> Bool {
-        if let startDate = startDateMaybe, endDate = endDateMaybe {
-            return startDate.hour() == startHour && startDate.minutes() == startMinutes && endDate.hour() == endHour && endDate.minutes() == endMinutes && titleMaybe == title
-        } else {
-            return false
-        }
-    }
-
-    // Decode
-    static func decode(dictionary: NSDictionary) -> FavoriteEvent {
-        let startHour = dictionary["startHour"] as! Int
-        let startMinutes = dictionary["startMinutes"] as! Int
-        let endHour = dictionary["endHour"] as! Int
-        let endMinutes = dictionary["endMinutes"] as! Int
-        let title = dictionary["title"] as? String
-        return FavoriteEvent(startHour: startHour, startMinutes: startMinutes, endHour: endHour, endMinutes: endMinutes, title: title)
-    }
-}
-
 class ViewController: UIViewController, WeekViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var monthLabel: UILabel!
